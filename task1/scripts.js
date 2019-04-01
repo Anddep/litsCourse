ade8cc87a7b8263b657e4d5f0492394e7ae60e19
Andrii Deputovych <a.deputovich@gmail.com>
2019-04-01 14:23:27 +0300

    finish task

--- task1/scripts.js
+++ task1/scripts.js
@@ -3,12 +3,14 @@
   const button = document.getElementById('openModal');
   const modal = document.getElementById('modal');
   const close = document.getElementById('close');
+  const selectAnimal = document.getElementById('favAnimal');
 
 
   year.innerHTML = new Date().getFullYear();
 
   button.addEventListener('click', function() {
     modal.show();
+    selectAnimal.focus();
   });
 
 
