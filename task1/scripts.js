7ea5aaabcbdac956044c3ed50e3ddbae0ce5bfb9
Andrii Deputovych <a.deputovich@gmail.com>
2019-04-03 18:33:20 +0300

    add template for practice

--- task1/scripts.js
+++ task1/scripts.js
@@ -1,23 +1,4 @@
 (function() {
-  const year = document.getElementById('year');
-  const button = document.getElementById('openModal');
-  const modal = document.getElementById('modal');
-  const close = document.getElementById('close');
-  const selectAnimal = document.getElementById('favAnimal');
-
-
-  year.innerHTML = new Date().getFullYear();
-
-  button.addEventListener('click', function() {
-    modal.show();
-    selectAnimal.focus();
-  });
-
-
-  close.addEventListener('click', function() {
-    modal.close();
-  });
-
 
 })
 ();
